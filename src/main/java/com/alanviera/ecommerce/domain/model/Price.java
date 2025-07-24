4456158ef56181a27a9ba0a865d5939012ff6d9f
Alan Viera <alanviera@hotmail.com>
2025-07-24 20:34:12 +0200

    feat(domain): add Price domain model entity

--- src/main/java/com/alanviera/ecommerce/domain/model/Price.java
+++ src/main/java/com/alanviera/ecommerce/domain/model/Price.java
@@ -1,4 +1,18 @@
 package com.alanviera.ecommerce.domain.model;
 
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
 public class Price {
+    private final Long productId;
+    private final Long brandId;
+    private final Long priceListId;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+    private final byte priority;
+    private final BigDecimal price;
+    private final String currency;
 }
