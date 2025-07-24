6df1831e8f8eb27cf4d77d1b79db00be24dace3b
Alan Viera <alanviera@hotmail.com>
2025-07-24 21:10:05 +0200

    feat(domain): add PriceQueryUseCase, PriceQueryUseCaseImpl, and PriceRepositoryPort classes

--- src/main/java/com/alanviera/ecommerce/domain/port/out/PriceRepositoryPort.java
+++ src/main/java/com/alanviera/ecommerce/domain/port/out/PriceRepositoryPort.java
@@ -1,4 +1,10 @@
 package com.alanviera.ecommerce.domain.port.out;
 
+import com.alanviera.ecommerce.domain.model.Price;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PriceRepositoryPort {
+    List<Price> findPricesByDateAndProductAndBrand(LocalDateTime date, Long productId, Long brandId);
 }
