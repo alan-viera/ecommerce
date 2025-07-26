a5518685eba20aefd0bb70861f82e9b92d057665
Alan Viera <alanviera@hotmail.com>
2025-07-26 18:45:21 +0200

    feat(adapter-in): add Jakarta validations on PriceRequestDto, add ValidationExceptionHandler class to handle MethodArgumentNotValidExceptions

--- src/main/java/com/alanviera/ecommerce/adapter/in/rest/dto/PriceRequestDto.java
+++ src/main/java/com/alanviera/ecommerce/adapter/in/rest/dto/PriceRequestDto.java
@@ -1,5 +1,8 @@
 package com.alanviera.ecommerce.adapter.in.rest.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PriceRequestDto {
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime date;
+
+    @NotNull
+    @Min(1)
     private Long productId;
+
+    @NotNull
+    @Min(1)
     private Long brandId;
 }
